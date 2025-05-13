2bc23e5ddd4a81031151385acd7cf150e6a6d6a9
gabrielDuarte <biel111107@gmail.com>
2025-05-13 11:10:29 -0300

    falta designer

--- cantinaPainel/obj/Debug/net8.0-windows/cantinaPainel.AssemblyInfo.cs
+++ cantinaPainel/obj/Debug/net8.0-windows/cantinaPainel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cantinaPainel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef4a791cf1ab4ad36e5a7b447768335532534a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8789e3f2a61cdff173beb8843debf94878bed406")]
 [assembly: System.Reflection.AssemblyProductAttribute("cantinaPainel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cantinaPainel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
